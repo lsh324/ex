e4d746dba13a85c3ffa88427d127f0ac35993db5
lsh324 <83631827+lsh324@users.noreply.github.com>
2021-10-25 13:16:48 +0900

    Add files via upload

--- portfolio/script/project.js
+++ portfolio/script/project.js
@@ -1,10 +1,19 @@
-const imageWidth = 60;
 const slideSpeed = 200;
 const slideList = document.querySelector('.cardwrap');
 const slideObj = document.querySelectorAll('.card');
+// const imageWidth = (document.querySelector('.card').offsetWidth/document.body.scrollWidth) * 100;
+const imageWidth = document.querySelector('.card').offsetWidth + (document.body.scrollWidth * 0.1);
+
+
+slideList.style.left = document.querySelector('.card').offsetWidth *0.5 + 'px';
+  // document.querySelector('.card').offsetWidth + (document.body.scrollWidth * 0.1)
+//left값
+
+// slideList.style.left = cardWidth * 0.5;
 const btnPrev = document.querySelector('.slide-btn-prev');
 const btnNext = document.querySelector('.slide-btn-next');
 
+// console.log(document.querySelector('.card').offsetWidth/document.body.scrollWidth);
 const slideLEN = slideObj.length;
 let curIndex = 0;
 
@@ -17,7 +26,7 @@ function clickBtnNext(){
     slideObj[curIndex].classList.add('on');
     slideObj[curIndex-1].classList.remove('on');
     slideList.style.transition = '300ms';
-    slideList.style.transform = 'translateX(-' + (imageWidth *curIndex+1) + 'vw)';
+    slideList.style.transform = 'translateX(-' + (imageWidth * curIndex + 1) + 'px)';
   if(curIndex === slideLEN-1){
     btnNext.style.color = 'rgba(255, 255, 255, 0.3)';
   }
@@ -32,7 +41,7 @@ function clickBtnPrev(){
     slideObj[curIndex].classList.add('on');
     slideObj[curIndex+1].classList.remove('on');
     slideList.style.transform = '300ms';
-    slideList.style.transform = 'translateX(-' + (imageWidth * curIndex+1) +'vw)';
+    slideList.style.transform = 'translateX(-' + (imageWidth * curIndex+1) +'px)';
 
 if(curIndex === 0){
   btnPrev.style.color = 'rgba(255, 255, 255, 0.3)';
@@ -48,7 +57,7 @@ function btnEvent(){
 function init(){
   btnEvent(); //버튼 이벤트 함수 호출
   //slide_list width 설정
-  slideList.style.width = (imageWidth * slideLEN) + 'vw';
+  slideList.style.width = (imageWidth * slideLEN) + 'px';
   slideList.style.transform = 'translate(0px)';
   btnPrev.style.color = 'rgba(255, 255, 255, 0.3)';
   slideObj[curIndex].classList.add('on');
